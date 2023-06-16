25fcb85bc4c2ebfc58c85886033b5edf5bb15003
gardarjuto <gi241@cam.ac.uk>
2023-06-16 12:36:22 +0100

    feat: add placeholder multi agent emitter

---
+++ qdax/core/emitters/ma_standard_emitters.py
@@ -0,0 +1,107 @@
+from functools import partial
+from typing import Callable, Optional, Tuple
+
+import jax
+import jax.numpy as jnp
+
+from qdax.core.containers.repertoire import Repertoire
+from qdax.core.emitters.emitter import Emitter, EmitterState
+from qdax.types import Genotype, RNGKey
+
+
+class MultiAgentMixingEmitter(Emitter):
+    def __init__(
+        self,
+        mutation_fn: Callable[[Genotype, RNGKey], Tuple[Genotype, RNGKey]],
+        variation_fn: Callable[[Genotype, Genotype, RNGKey], Tuple[Genotype, RNGKey]],
+        variation_percentage: float,
+        batch_size: int,
+        num_agents: int,
+        agents_to_mutate: int,
+    ) -> None:
+        self._mutation_fn = mutation_fn
+        self._variation_fn = variation_fn
+        self._variation_percentage = variation_percentage
+        self._batch_size = batch_size
+        self._num_agents = num_agents
+        self._agents_to_mutate = agents_to_mutate
+
+    @partial(
+        jax.jit,
+        static_argnames=("self",),
+    )
+    def emit(
+        self,
+        repertoire: Repertoire,
+        emitter_state: Optional[EmitterState],
+        random_key: RNGKey,
+    ) -> Tuple[Genotype, RNGKey]:
+        """
+        Emitter that performs both mutation and variation. Two batches of
+        variation_percentage * batch_size genotypes are sampled in the
+        repertoire, copied and cross-over to obtain new offsprings. One batch
+        of (1.0 - variation_percentage) * batch_size genotypes are sampled in
+        the repertoire, copied and mutated.
+
+        Note: this emitter has no state. A fake none state must be added
+        through a function redefinition to make this emitter usable with
+        MAP-Elites.
+
+        Params:
+            repertoire: the MAP-Elites repertoire to sample from
+            emitter_state: void
+            random_key: a jax PRNG random key
+
+        Returns:
+            a batch of offsprings
+            a new jax PRNG key
+        """
+        # Indices of agents to mutate or vary
+        # agent_indices = jax.random.choice(
+        #   random_key,
+        #   self._num_agents,
+        #   (self._agents_to_mutate,),
+        #   replace=False
+        # )
+
+        n_variation = int(self._batch_size * self._variation_percentage)
+        n_mutation = self._batch_size - n_variation
+
+        if n_variation > 0:
+            x1, random_key = repertoire.sample(random_key, n_variation)
+            x2, random_key = repertoire.sample(random_key, n_variation)
+
+            x_variation, random_key = self._variation_fn(x1, x2, random_key)
+
+            # Put back agents in their original positions
+            # mask = jnp.zeros(self._num_agents, dtype=bool).at[agent_indices].set(True)
+            # x_variation = jnp.where(mask, x1, x_variation)
+
+        if n_mutation > 0:
+            x1, random_key = repertoire.sample(random_key, n_mutation)
+            x_mutation, random_key = self._mutation_fn(x1, random_key)
+
+            # Put back agents in their original positions
+            # mask = jnp.zeros(self._num_agents, dtype=bool).at[agent_indices].set(True)
+            # x_mutation = jnp.where(mask, x1, x_mutation)
+
+        if n_variation == 0:
+            genotypes = x_mutation
+        elif n_mutation == 0:
+            genotypes = x_variation
+        else:
+            genotypes = jax.tree_util.tree_map(
+                lambda x_1, x_2: jnp.concatenate([x_1, x_2], axis=0),
+                x_variation,
+                x_mutation,
+            )
+
+        return genotypes, random_key
+
+    @property
+    def batch_size(self) -> int:
+        """
+        Returns:
+            the batch size emitted by the emitter.
+        """
+        return self._batch_size

e316e025eccb78cafe1072acba52a521c1b9ab2b
Hải Đăng <haidanggacon@gmail.com>
2025-07-22 19:22:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,17 +22,17 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "absl-py>=1.0.0",
-        "jax>=0.3.16",
-        "jaxlib>=0.3.15",  # necessary to build the doc atm
+        "jax==0.4.10",
+        "jaxlib==0.4.10",  # necessary to build the doc atm
         "jinja2<3.1.0",
-        "jumanji>=0.1.3",
-        "flax>=0.6, <0.6.2",
-        "brax>=0.0.15",
-        "gym>=0.23.1",
-        "numpy>=1.22.3",
-        "optax>=0.1, <0.1.5",
+        "jumanji==0.3.1",
+        "flax==0.8.5",
+        "brax==0.10.4",
+        "gym==0.23.1",
+        "numpy==1.24.1",
+        "optax==0.1.9",
         "scikit-learn>=1.0.2",
-        "scipy>=1.8.0",
+        "scipy==1.9.0",
     ],
     dependency_links=[
         "https://storage.googleapis.com/jax-releases/jax_releases.html",
